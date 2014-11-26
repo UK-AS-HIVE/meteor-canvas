929795e875eaacf69275ddd90607044e66a9897c
Michael Paddock <mpaddock@gmail.com>
2014-11-26 15:58:20 -0500

    Upgrade to 1.0, node package gone, new server OAuth functions.

---
+++ server/canvas_server.js
@@ -0,0 +1,46 @@
+var getCode = function(query) {
+  //Sends a GET request to the canvas installation for a code. (Step 1 in Canvas OAuth flow)
+  var config = Meteor.settings.canvasConfig;
+  if(!config) 
+    throw new ServiceConfiguration.ConfigError();
+
+  var responseContent;
+  try {
+    responseContent = HTTP.get(config.canvasURL + '/login/oauth2/auth', {
+      params: {
+        client_id: config.client_id,
+        response_type: code,
+        redirect_uri: config.redirectUri
+      }
+    }.content;
+  } catch (err) {
+    throw _.extend(new Error("Failed to complete OAuth handshake with Canvas. " + err.message),
+                    {response: err.response});
+  }
+  return responseContent;
+};
+
+var getAccessToken = function (query) {
+  //Sends a POST request for the OAuth access token for our user. (Step 3)
+  var config = Meteor.settings.canvasConfig;
+  try {
+    responseContent = HTTP.post(config.canvasURL + '/login/oauth/token', {
+      params: {
+        client_id: config.clientId,
+        redirect_uri: config.redirectUri,
+        client_secret: OAuth.openSecret(config.secret),
+        code: query.code
+      }
+    }).content;
+  console.log(responseContent);
+  } catch (err) {
+    throw _.extend(new Error("Failed to complete OAuth handshake with Canvas. " + err.message),
+                    {response: err.response});
+  }
+
+  if(!isJSON(responseContent)) {
+    throw new Error("Failed to complete OAuth handshake, response was of invalid type. " + responseContent);
+  }
+
+  return responseContent.accessToken;
+}
