b73335ace561f4f6a19bc4d14ce9ac3c0ad31f22
mpaddock <mpaddock@gmail.com>
2014-11-29 19:22:58 -0500

    More OAuth progress...

---
+++ client/canvas_client.js
@@ -0,0 +1,40 @@
+Canvas = {};
+
+Canvas.requestCredential = function (options, credentialRequestCompleteCallback) {
+  if (!credentialRequestCompleteCallback && typeof options === 'function') {
+    credentialRequestCompleteCallback = options;
+    options = {};
+  }
+
+  var config = Meteor.settings.canvasConfig;
+  if (!config) {
+    credentialRequestCompleteCallback && credentialRequestCompleteCallback(
+    new ServiceConfiguration.ConfigError());
+    return;
+  }
+
+  var credentialToken = Random.secret();
+  //This is the arbitrary piece of state that we pass to Canvas for verification.
+
+  var loginStyle = OAuth._loginStyle('canvas', config, options);
+
+  var loginPath = '_oauth/canvas/?requestTokenAndRedirect=true'
+        + '&state=' +OAuth._stateParam(loginStyle,credentialToken);
+
+  if (Meteor.isCordova) {
+    loginPath = loginPath + "&cordova=true";
+    if(/Android/i.test(navigator.userAgent)) {
+      loginPath = loginPath + "&android=true";
+    }
+  }
+
+  var loginUrl = Meteor.absoluteUrl(loginPath);
+
+  OAuth.launchLogin({
+    loginService: "canvas",
+    loginStyle: loginStyle,
+    loginUrl: loginUrl,
+    credentialRequestCompleteCallback: credentialRequestCompleteCallback,
+    credentialToken: credentialToken 
+  });
+};
